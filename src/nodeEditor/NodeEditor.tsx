9805c1ee8cfb971e45a10730aabf09fa7c96d917
Lexyna <lexieverena@outlook.de>
2022-10-13 20:44:39 +0200

    Implemented zoom function

--- src/nodeEditor/NodeEditor.tsx
+++ src/nodeEditor/NodeEditor.tsx
@@ -1,5 +1,5 @@
 import { ConfigNodeDict, EngineNodeDict } from "graph.exe-core";
-import React, { CSSProperties, useRef } from "react";
+import React, { CSSProperties, useRef, useState, WheelEvent } from "react";
 import { ConnectionStage } from "../Connections/ConnectionsStage";
 
 const nodeEditorCSS: CSSProperties = {
@@ -14,13 +14,26 @@ export const NodeEditor = (props: NodeEditorProps) => {
 
     const editorRef = useRef<HTMLDivElement>(null);
 
+    const [zoom, setZoom] = useState(1);
+
+    const zoomListener = (e: WheelEvent) => {
+        let newZoom = zoom;
+
+        if (e.deltaY > 0) newZoom += 0.05;
+        else newZoom -= 0.05;
+
+        if (newZoom < 0.3 || newZoom > 1.2) return
+        setZoom(newZoom);
+    }
+
     return (
         <div
             ref={editorRef}
             style={nodeEditorCSS}
+            onWheel={zoomListener}
         >
             <ConnectionStage
-                zoom={1}
+                zoom={zoom}
                 editorOffset={{ x: 0, y: 0 }}
                 dragOffset={{ x: 0, y: 0 }}
             ></ConnectionStage>
