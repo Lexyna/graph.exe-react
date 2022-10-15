706cd2de319f7c868c7c8d8855508643a47013fb
Lexyna <lexieverena@outlook.de>
2022-10-15 17:08:06 +0200

    Added hover effect for connections

--- src/Connections/Connection.tsx
+++ src/Connections/Connection.tsx
@@ -1,16 +1,21 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useState } from "react";
 
 export const Connection = (props: ConnectionProps) => {
+
+    const [strokeWidth, setStrokeWidth] = useState<number>(2);
 
     const connectionStyle: CSSProperties = {
         transform: `scale(${props.zoom})`,
         stroke: props.color,
-        strokeWidth: 2,
+        strokeWidth: strokeWidth,
         strokeDasharray: props.dashArray ? props.dashArray : "",
     };
 
+    const onHover = () => { setStrokeWidth(6) }
+    const onHoverLeave = () => { setStrokeWidth(2) }
+
     return (
-        <svg>
+        <svg onMouseEnter={onHover} onMouseLeave={onHoverLeave}>
             <path
                 style={connectionStyle}
                 fill="none"
