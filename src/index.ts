a1273b544dbd2d1dbdf180dfe3ebf6d701109824
Lexyna <lexieverena@outlook.de>
2022-10-22 09:45:49 +0200

    Created type exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-export * from "./nodeEditor";
+import { GridOptions } from "./Connections/Grid";
+import { NodeEditor } from "./nodeEditor";
+import { ExtraProps, ProtoIO, ProtoIOStyle } from "./ProtoTypes/ProtoIO";
+import { ProtoEngineNode, ProtoEngineNodeDict, ProtoNode, ProtoNodeDict, ProtoNodeStyle } from "./ProtoTypes/ProtoNode";
+import { buildEngineNode } from "./Utils/utils";
 
+export {
+    NodeEditor,
+    ProtoNode,
+    ProtoNodeStyle,
+    ProtoNodeDict,
+    ProtoEngineNode,
+    ProtoEngineNodeDict,
+    ProtoIO,
+    ProtoIOStyle,
+    ExtraProps,
+    GridOptions,
+    buildEngineNode
+};
